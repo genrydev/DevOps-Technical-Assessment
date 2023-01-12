972e159f606a2563604a5e8c9d97b9b223fb3e1b
Genry Leyva <gleyvaglez@gmail.com>
2023-01-12 15:35:16 -0300

    tf init

---
+++ infraestructure-deployment/variables.tf
@@ -0,0 +1,12 @@
+variable "location_zone" {
+  default     = "eastus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  default     = "rg-dta"
+  description = "DevOps Technical Assessment resource group name."
+}
+
+
+
