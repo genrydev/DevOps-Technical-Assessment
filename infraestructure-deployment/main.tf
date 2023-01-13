d164de5860ed638c38f75f7b0c0acf1d06a5888d
Genry <gleyvaglez@gmail.com>
2023-01-12 22:00:35 -0300

    deploy acr

--- infraestructure-deployment/main.tf
+++ infraestructure-deployment/main.tf
@@ -2,3 +2,11 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location_zone
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "glgacr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
