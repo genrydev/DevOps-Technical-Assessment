308d6952d75dd5ed25482c7e76cd740dbb0de2ce
Genry Leyva <gleyvaglez@gmail.com>
2023-02-01 00:50:44 -0300

    add api_key dependency

--- main_api_app.py
+++ main_api_app.py
@@ -1,9 +1,9 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Security, HTTPException
 from pydantic import BaseModel, BaseSettings
 from fastapi.responses import JSONResponse
-from fastapi.security.api_key import APIKey
+from fastapi.security.api_key import APIKeyHeader
+from starlette.status import HTTP_403_FORBIDDEN
 import os
-import auth
 
 class Item(BaseModel):
     message: str
@@ -19,9 +19,20 @@ APP_VERSION = os.getenv('APP_VERSION')
 INSTANCE_ID = os.getenv('HOSTNAME')
 headers_json = { "X-App-Version": APP_VERSION, "X-Container-Instance-Id": INSTANCE_ID }
 
+api_key = os.getenv('API_KEY')
+api_key_header = APIKeyHeader(name="X-Parse-REST-API-Key", auto_error=False)
+
+async def get_api_key(api_key_header: str = Security(api_key_header)):
+    if api_key_header == api_key:
+        return api_key_header
+    else:
+        raise HTTPException(
+            status_code=HTTP_403_FORBIDDEN, detail="Could not validate API KEY"
+        )
+
 app = FastAPI(openapi_url=settings.openapi_url)
 
-@app.post("/DevOps", dependencies=[Depends(auth.get_api_key)])
+@app.post("/DevOps", dependencies=[Depends(get_api_key)])
 async def create_item(item: Item):
     response_message = f"Hello {item.to} your messaje will be send"
     content = { "message": response_message }
